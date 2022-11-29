8cc8915c3648ae0b4a173fa70ca74b7ef4886963
Minh Chau <buitongminhchau01032002@gmail.com>
2022-11-29 21:30:02 +0700

    image for product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/Product');
+const imageToolkit = require('../utils/imageToolkit');
 
 // [GET] api/product
 const read = async (req, res, next) => {
@@ -14,11 +15,23 @@ const read = async (req, res, next) => {
 
 // [POST] api/product
 const create = async (req, res, next) => {
-    const { name, price, type } = req.body;
-
+    const { name, price, type, image } = req.body;
     // Validate field
     if (!name || !price || !type) {
         return res.status(400).json({ success: false, status: 400, message: 'Missed field' });
+    }
+
+    // upload image
+    let imageResult;
+    if (image) {
+        try {
+            imageResult = await imageToolkit.upload(image);
+            if (!imageResult) {
+                return res.status(500).json({ success: false, status: 500, message: 'Internal server error' });
+            }
+        } catch (err) {
+            return res.status(500).json({ success: false, status: 500, message: 'Internal server error' });
+        }
     }
 
     try {
@@ -26,6 +39,7 @@ const create = async (req, res, next) => {
             name,
             price,
             type,
+            image: image && imageResult.secure_url
         });
         await newProduct.save();
         return res.status(201).json({ success: true, product: newProduct });
@@ -40,7 +54,7 @@ const readOne = async (req, res, next) => {
     const id = req.params.id;
     try {
         let product;
-        product = await Product.findOne({id}).populate('type');
+        product = await Product.findOne({ id }).populate('type');
         return res.status(200).json({ success: true, product });
     } catch (err) {
         console.log(err);
@@ -48,25 +62,38 @@ const readOne = async (req, res, next) => {
     }
 };
 
-
 // [PUT] api/product/:id
 const update = async (req, res, next) => {
     const id = Number(req.params.id);
     const bodyObj = req.body;
     const updateObj = {};
-    console.log(bodyObj)
+    console.log(bodyObj);
 
-    Object.keys(bodyObj).forEach(key => {
+    Object.keys(bodyObj).forEach((key) => {
         if (bodyObj[key] !== undefined) {
             updateObj[key] = bodyObj[key];
         }
     });
 
+    // Upload image
+    if (updateObj.image) {
+        let imageResult;
+        try {
+            imageResult = await imageToolkit.upload(image);
+            if (!imageResult) {
+                return res.status(500).json({ success: false, status: 500, message: 'Internal server error' });
+            }
+        } catch (err) {
+            return res.status(500).json({ success: false, status: 500, message: 'Internal server error' });
+        }
+        updateObj.image = imageResult.secure_url || 'https://picsum.photos/200/300';
+    }
+
     // Update product
     try {
-        const newProduct = await Product.findOneAndUpdate({id}, updateObj, {
+        const newProduct = await Product.findOneAndUpdate({ id }, updateObj, {
             new: true,
-        })
+        });
         return res.status(200).json({ success: true, product: newProduct });
     } catch (err) {
         console.log(err);
@@ -75,3 +102,4 @@ const update = async (req, res, next) => {
 };
 
 module.exports = { read, create, readOne, update };
+ 
--- src/models/Product.js
+++ src/models/Product.js
@@ -19,6 +19,14 @@ const ProductSchema = new Schema(
             type: Schema.Types.ObjectId,
             ref: 'product_types',
         },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1,
+        },
+        image: {
+            type: String,
+        },
     },
     {
         timestamps: true,
