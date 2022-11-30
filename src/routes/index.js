2cc8b9da8d67216a95cdb496f26d092420980557
Minh Chau <buitongminhchau01032002@gmail.com>
2022-11-30 11:28:41 +0700

    add customer

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 const productRoute = require('./product');
 const productTypeRoute = require('./productType');
+const customerRoute = require('./customer');
 
 router.use('/product', productRoute);
 router.use('/product-type', productTypeRoute);
+router.use('/customer', customerRoute);
 
 module.exports = router;
