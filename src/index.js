6b97ae3f331833037f32e44efe535efb13388921
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-04 16:42:44 +0700

    find -> aggregate, add filters

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var queryType = require('query-types');
 require('dotenv').config();
 const corsConfig = require('./configs/cors');
 const connectDB = require('./configs/db');
@@ -12,6 +13,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(corsConfig);
 
+app.use(queryType.middleware());
 
 // route
 app.use('/api', route);
