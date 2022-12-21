995677c063b5e81d58b1ecba99b85aa24e1e8f94
Minh Chau <buitongminhchau01032002@gmail.com>
2022-12-22 00:30:34 +0700

    update detail order

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
-
 const express = require('express');
 const router = express.Router();
 const productRoute = require('./product');
 const productTypeRoute = require('./productType');
 const customerRoute = require('./customer');
 const orderRoute = require('./order');
+const detailOrder = require('./detailOrder');
 
 router.use('/product', productRoute);
 router.use('/product-type', productTypeRoute);
 router.use('/customer', customerRoute);
 router.use('/order', orderRoute);
+router.use('/detail-order', detailOrder);
 
 module.exports = router;
